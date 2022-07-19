8efabc7bfe4776e4336b6321fd53a4e0ac5e8c67
DiOnFire <di0nik.gd@gmail.com>
2022-07-19 15:32:39 +0300

    Firebase phone implementation part 1: base code and listeners

--- app/src/main/java/org/kapyteam/messenger/database/FirebaseAuthAgent.kt
+++ app/src/main/java/org/kapyteam/messenger/database/FirebaseAuthAgent.kt
@@ -6,17 +6,28 @@
 package org.kapyteam.messenger.database
 
 import android.app.Activity
+import android.widget.Toast
+import androidx.annotation.NonNull
 import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.PhoneAuthCredential
 import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import org.kapyteam.messenger.traits.Profile
 import java.util.concurrent.TimeUnit
 
 class FirebaseAuthAgent {
     companion object {
         @JvmStatic
         private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+        @JvmStatic
+        private var dbReference = FirebaseDatabase
+            .getInstance()
+            .getReferenceFromUrl("https://kapy-messenger-default-rtdb.firebaseio.com/")
 
         fun getInstance(): FirebaseAuth = auth
 
@@ -29,6 +40,26 @@ class FirebaseAuthAgent {
                 TimeUnit.SECONDS,
                 activity,
                 verifyCallback())
+        }
+
+        fun registerProfile(profile: Profile) {
+            dbReference.addListenerForSingleValueEvent(
+                object : ValueEventListener {
+                    override fun onDataChange(@NonNull snapshot: DataSnapshot) {
+                        snapshot.child("users").let {
+                            if (it.hasChild(profile.phone) || it.hasChild(profile.nickname)) {
+
+                            } else {
+
+                            }
+                        }
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        TODO("Not yet implemented")
+                    }
+                }
+            )
         }
 
         private fun verifyCallback() =
