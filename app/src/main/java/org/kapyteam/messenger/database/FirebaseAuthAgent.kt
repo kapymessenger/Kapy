f18c160dca580e47240806d88d5271ecebcd069b
DiOnFire <di0nik.gd@gmail.com>
2022-07-19 12:15:35 +0300

    Added google-services json

--- app/src/main/java/org/kapyteam/messenger/database/FirebaseAuthAgent.kt
+++ app/src/main/java/org/kapyteam/messenger/database/FirebaseAuthAgent.kt
@@ -5,17 +5,54 @@
 
 package org.kapyteam.messenger.database
 
+import android.app.Activity
+import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
 
 class FirebaseAuthAgent {
     companion object {
-        @JvmStatic private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+        @JvmStatic
+        private var auth: FirebaseAuth = FirebaseAuth.getInstance()
 
         fun getInstance(): FirebaseAuth = auth
 
         fun getCurrentUser(): FirebaseUser? = auth.currentUser
 
-        fun phoneAuth(): Boolean = TODO("Not implemented")
+        fun phoneAuth(phone: String, activity: Activity) {
+            PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phone,
+                120,
+                TimeUnit.SECONDS,
+                activity,
+                verifyCallback(),
+
+                )
+        }
+
+        private fun verifyCallback() =
+            object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                override fun onVerificationCompleted(
+                    credential: PhoneAuthCredential
+                ) {
+
+                }
+
+                override fun onVerificationFailed(
+                    e: FirebaseException
+                ) {
+
+                }
+
+                override fun onCodeSent(
+                    id: String,
+                    token: PhoneAuthProvider.ForceResendingToken
+                ) {
+
+                }
+            }
     }
 }
