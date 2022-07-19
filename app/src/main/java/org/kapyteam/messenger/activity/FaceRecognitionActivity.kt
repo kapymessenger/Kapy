8371274a27c9ffd2b8cdc430854af0bcb1efc0c3
Yuriy007y <arisov007y@gmail.com>
2022-07-19 15:00:16 +0300

    Recognizer

--- app/src/main/java/org/kapyteam/messenger/activity/FaceRecognitionActivity.kt
+++ app/src/main/java/org/kapyteam/messenger/activity/FaceRecognitionActivity.kt
@@ -9,6 +9,7 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.text.Html
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -19,6 +20,8 @@ import org.kapyteam.messenger.databinding.ActivityFaceRecognitionBinding
 import org.kapyteam.messenger.ml.ModelUnquant
 import org.tensorflow.lite.DataType
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 class FaceRecognitionActivity : AppCompatActivity() {
 
@@ -26,10 +29,13 @@ class FaceRecognitionActivity : AppCompatActivity() {
     private lateinit var button: Button
     private lateinit var resText: TextView
 
+    private val imageSize = 224
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFaceRecognitionBinding.inflate(layoutInflater)
         val view = binding.root
+        setContentView(view)
 
         button = binding.photoButton
         resText = binding.resultText
@@ -54,23 +60,55 @@ class FaceRecognitionActivity : AppCompatActivity() {
 
     private val takePicturePreview = registerForActivityResult(ActivityResultContracts.TakePicturePreview()){bitmap ->
         if(bitmap != null){
-            //imageView
+            outputGenerator(bitmap)
         }
     }
 
-//    private fun outputGenerator(bitmap: Bitmap){
-//        val model = ModelUnquant.newInstance(this)
-//
-//// Creates inputs for reference.
-//        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
-//        inputFeature0.loadBuffer(byteBuffer)
-//
-//// Runs model inference and gets result.
-//        val outputs = model.process(inputFeature0)
-//        val outputFeature0 = outputs.outputFeature0AsTensorBuffer
-//
-//// Releases model resources if no longer used.
-//        model.close()
-//
-//    }
+    private fun outputGenerator(bitmap: Bitmap){
+        val model = ModelUnquant.newInstance(this)
+
+// Creates inputs for reference.
+        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+        val byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3)
+        byteBuffer.order(ByteOrder.nativeOrder())
+        val intValues = IntArray(imageSize * imageSize)
+        bitmap!!.getPixels(intValues, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+        var pixel = 0
+        for (i in 0 until imageSize) {
+            for (j in 0 until imageSize) {
+                val `val` = intValues[pixel++]
+                byteBuffer.putFloat((`val` shr 16 and 0xFF) * (1f / 255f))
+                byteBuffer.putFloat((`val` shr 8 and 0xFF) * (1f / 255f))
+                byteBuffer.putFloat((`val` and 0xFF) * (1f / 255f))
+            }
+        }
+
+        inputFeature0.loadBuffer(byteBuffer)
+
+        val outputs = model.process(inputFeature0)
+        val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+        val confidences: FloatArray = outputFeature0.floatArray
+        var maxPos = 0
+        var maxConfidence = 0f
+        for (i in confidences.indices) {
+            if (confidences[i] > maxConfidence) {
+                maxConfidence = confidences[i]
+                maxPos = i
+            }
+        }
+
+        println(confidences[2])
+        val classes = arrayOf("😳", "😲", "😑", "🙂", "😃")
+        //resText!!.text = classes[maxPos]
+
+        var s = ""
+        for (i in classes.indices) {
+            s += String.format("%s: %.1f%%\n", classes[i], confidences[i] * 100)
+        }
+        resText.text = s
+// Releases model resources if no longer used.
+        model.close()
+
+    }
 }
