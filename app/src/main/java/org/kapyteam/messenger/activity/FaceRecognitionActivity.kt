3b8cfe587cfbf8182770ed0a19ec8c7bf193e086
Yuriy007y <arisov007y@gmail.com>
2022-07-19 12:49:26 +0300

    Recognizer

--- app/src/main/java/org/kapyteam/messenger/activity/FaceRecognitionActivity.kt
+++ app/src/main/java/org/kapyteam/messenger/activity/FaceRecognitionActivity.kt
@@ -5,19 +5,26 @@
 
 package org.kapyteam.messenger.activity
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import org.kapyteam.messenger.databinding.ActivityFaceRecognitionBinding
+import org.kapyteam.messenger.ml.ModelUnquant
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 
 class FaceRecognitionActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFaceRecognitionBinding
     private lateinit var button: Button
     private lateinit var resText: TextView
-    private val GALLERY_REQUEST_CODE = 123
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +34,43 @@ class FaceRecognitionActivity : AppCompatActivity() {
         button = binding.photoButton
         resText = binding.resultText
 
-        //Я еще не дописал не трогайте это и одноименнуб активность, пж.
+        button.setOnClickListener{
+            if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED)
+            {
+                takePicturePreview.launch(null)
+            }
+            else{
+                requestedPermission.launch(android.Manifest.permission.CAMERA)
+            }
+        }
+
     }
+    private val requestedPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()){granted->
+        if(granted){
+            takePicturePreview.launch(null)
+        }
+    }
+
+    private val takePicturePreview = registerForActivityResult(ActivityResultContracts.TakePicturePreview()){bitmap ->
+        if(bitmap != null){
+            //imageView
+        }
+    }
+
+//    private fun outputGenerator(bitmap: Bitmap){
+//        val model = ModelUnquant.newInstance(this)
+//
+//// Creates inputs for reference.
+//        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+//        inputFeature0.loadBuffer(byteBuffer)
+//
+//// Runs model inference and gets result.
+//        val outputs = model.process(inputFeature0)
+//        val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+//
+//// Releases model resources if no longer used.
+//        model.close()
+//
+//    }
 }
