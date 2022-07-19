a7d0c3b72025154eb21a4b7ab37a8436bb940f30
Mrsuper125 <Mrsuper1221@gmail.com>
2022-07-19 15:05:45 +0300

    first registration screen (telephone number)

--- app/src/main/java/org/kapyteam/messenger/activity/GreetingActivity.kt
+++ app/src/main/java/org/kapyteam/messenger/activity/GreetingActivity.kt
@@ -5,13 +5,24 @@
 
 package org.kapyteam.messenger.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import org.kapyteam.messenger.R
 
 class GreetingActivity : AppCompatActivity() {
+
+    private lateinit var sign_up_button :Button;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_greeting)
+
+        sign_up_button = findViewById(R.id.sign_up_button)
+        sign_up_button.setOnClickListener{
+            val intent = Intent(this, registartion1::class.java)
+            startActivity(intent)
+        }
     }
 }
