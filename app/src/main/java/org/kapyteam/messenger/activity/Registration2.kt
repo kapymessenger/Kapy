0e4abba171d20469fd318a8ab0bfe5ff69750b47
Mrsuper125 <Mrsuper16 is cool!>
2022-07-20 11:42:42 +0300

    completed otp

--- app/src/main/java/org/kapyteam/messenger/activity/Registration2.kt
+++ app/src/main/java/org/kapyteam/messenger/activity/Registration2.kt
@@ -1,14 +1,42 @@
 package org.kapyteam.messenger.activity
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.getSystemService
+import androidx.core.widget.addTextChangedListener
+import com.chaos.view.PinView
 import org.kapyteam.messenger.R
 
 class Registration2 : AppCompatActivity() {
+
+    private lateinit var verification_code: PinView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration2)
+        supportActionBar?.hide()
 
+        verification_code = findViewById(R.id.verification_code)
 
+        verification_code.requestFocus()
+        val inputMethodManager : InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED,InputMethodManager.HIDE_IMPLICIT_ONLY)
+
+        verification_code.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if(p0.toString().length == 6){}
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
     }
 }
