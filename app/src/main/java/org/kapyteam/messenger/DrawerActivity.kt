34e53678581292d5333e7d8d181dcd1e9b8aead6
Mrsuper125 <Mrsuper16 is cool!>
2022-07-20 17:09:21 +0300

    start of drawer making

---
+++ app/src/main/java/org/kapyteam/messenger/DrawerActivity.kt
@@ -0,0 +1,11 @@
+package org.kapyteam.messenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DrawerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drawer)
+    }
+}
