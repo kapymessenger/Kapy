1de5eb8a031c7648e79e97d59cd940fae7b62d04
Mrsuper125 <Mrsuper1221@gmail.com>
2022-07-21 11:39:27 +0300

    drawer is complete!

--- app/src/main/java/org/kapyteam/messenger/DrawerActivity.kt
+++ app/src/main/java/org/kapyteam/messenger/DrawerActivity.kt
@@ -2,10 +2,41 @@ package org.kapyteam.messenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
 
 class DrawerActivity : AppCompatActivity() {
+
+    private lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_drawer)
+
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawer_layout)
+        val navigationView : NavigationView = findViewById(R.id.navigation_view)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout,R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navigationView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.drawer_settings -> println("Settings")
+                R.id.drawer_contact -> println("Contact")
+                R.id.drawer_logout -> println("Log Out")
+                R.id.drawer_qr -> println("QR code")
+            }
+
+            true
+        }
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return toggle.onOptionsItemSelected(item)
     }
 }
