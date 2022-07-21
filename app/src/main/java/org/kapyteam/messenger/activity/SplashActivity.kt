8d679c96ca3605a698a22f5c725f07741155581d
DiOnFire <di0nik.gd@gmail.com>
2022-07-21 11:45:35 +0300

    auto activity changing

---
+++ app/src/main/java/org/kapyteam/messenger/activity/SplashActivity.kt
@@ -0,0 +1,31 @@
+package org.kapyteam.messenger.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.kapyteam.messenger.R
+import org.kapyteam.messenger.database.FirebaseAuthAgent
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        verifyAccount()
+    }
+
+    private fun verifyAccount() {
+        if (FirebaseAuthAgent.getCurrentUser() != null) {
+            val intent = Intent(
+                this,
+                MessengerActivity::class.java
+            )
+            startActivity(intent)
+        } else {
+            val intent = Intent(
+                this,
+                GreetingActivity::class.java
+            )
+            startActivity(intent)
+        }
+    }
+}
