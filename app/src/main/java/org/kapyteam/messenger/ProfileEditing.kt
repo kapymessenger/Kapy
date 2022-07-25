b6c43425b95bf85a35010725dc4c6db8520cdbe1
Mrsuper125 <Mrsuper1221@gmail.com>
2022-07-25 14:44:33 +0300

    added activity for profile editing and fixed EditText paddings

---
+++ app/src/main/java/org/kapyteam/messenger/ProfileEditing.kt
@@ -0,0 +1,11 @@
+package org.kapyteam.messenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileEditing : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_editing)
+    }
+}
