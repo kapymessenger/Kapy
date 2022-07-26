58e9ab9339042a8add91a18bde637279c4e20d79
Yuriy007y <arisov007y@gmail.com>
2022-07-26 13:22:51 +0300

    ignore list + text editor

---
+++ app/src/main/java/org/kapyteam/messenger/activity/TextEditor.kt
@@ -0,0 +1,12 @@
+package org.kapyteam.messenger.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.kapyteam.messenger.R
+
+class TextEditor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_text_editor)
+    }
+}
